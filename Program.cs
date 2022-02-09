2260a33d1584e562c8fc7fc314d7262db1a2ce90
mkhoatd <binnhatle@gmail.com>
2022-02-09 09:43:25 +0700

    change main and add Add and HV function

--- Program.cs
+++ Program.cs
@@ -4,34 +4,24 @@ namespace _20Nh15
 {
     class Program
     {
+        public static void HV(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+        public static void Add(out int c)
+        {
+            c = 5;
+        }
         static void Main(string[] args)
         {
-            // Console.WriteLine("DUT");
-            // Console.Write("ITF");
-            // int x = 1, y = 2;
-            // Console.WriteLine("x= " + x);
-            // Console.WriteLine("x= {0}", x);
-            // Console.WriteLine("x= {0}, y= {1}", x, y);
-            // Console.WriteLine("x= {0}, y= {1}", y, x);
-            // int x;
-            // Console.Write("Input x= ");
-            // x = Console.Read();
-            // Console.WriteLine("x= {0}", x);
-            // string y;
-            // Console.Write("Input y= ");
-            // y = Console.ReadLine();
-            // Console.WriteLine("y= {0}", y);
-            // int x = 100;
-            // object obj = new object(); //init new object, give a empty storage in heap
-            // Console.WriteLine(obj);
-            // obj = x; //boxing
-            // Console.Write(x);
-            //unboxing
-            object obj; //reference type
-            int x = 100;
-            obj = x; //boxing
-            int y = (int)obj; //unboxing, create new variable y, copy obj value in heap to y storage in stack
-            Console.WriteLine(y);
+            int t;
+            int m = 1, n = 2;
+            HV(ref m, ref n);
+            Console.WriteLine("m={0}, n={1}", m, n);
+            Add(out t);
+            Console.WriteLine("t={0}", t);
         }
     }
 }
